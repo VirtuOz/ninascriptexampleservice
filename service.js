f98b3dbdeba2269519d7574ea7f01baf75e3fcae
Kevan Dunsmore <kdunsmore@virtuoz.com>
2013-08-17 15:17:34 -0700

    Added click to call functionality

--- service.js
+++ service.js
@@ -17,17 +17,20 @@ app.get('/weather', function (req, res)
             city: req.query.city
         };
 
-        if (response.city == 'san francisco') {
+        if (response.city == 'san francisco')
+        {
             response.temperature = 71;
             response.outlook = 'sunny';
             response.knownCity = true;
         }
-        else if (response.city == 'edinburgh') {
+        else if (response.city == 'edinburgh')
+        {
             response.temperature = 40;
             response.outlook = 'rainy';
             response.knownCity = true;
         }
-        else {
+        else
+        {
             response.knownCity = false;
         }
 
@@ -35,19 +38,46 @@ app.get('/weather', function (req, res)
     }
 });
 
-app.get('/phonecall', function(req, res)
+var phoneCallHandler = function (req, res)
 {
     var twilio = require('twilio');
     var resp = new twilio.TwimlResponse();
 
-    resp.say('Welcome to Twilio!');
-    resp.say('Please let us know if we can help during your development.', {
-        voice:'woman',
-        language:'en-gb'
+    resp.say("Hi, this is Nina, calling from Citibank! Thanks for requesting a call back. Your live agent will be with you shortly.", {
+        voice: 'woman',
+        language: 'en-us'
     });
 
-    res.send(resp.toString());
-});
+    var liveAgentPhoneNumber = req.query.liveAgentPhoneNumber;
+    var phoneNumber = req.query.phoneNumber;
+
+    // Connect with the live agent operator.
+    if (liveAgentPhoneNumber && liveAgentPhoneNumber != phoneNumber)
+    {
+        resp.dial(liveAgentPhoneNumber);
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(resp.toString());
+};
+app.get('/phonecall', phoneCallHandler);
+app.post('/phonecall', phoneCallHandler);
+
+var smsHandler = function (req, res)
+{
+    var twilio = require('twilio');
+    var client = new twilio.RestClient('AC7b9b6eb19ff245fca1bf37fae1505147', 'ae87da74a5cfd04e53e91d0d3306d0cc');
+    var twilioSms = {
+        to: req.query.phoneNumber,
+        from: '15107358626',
+        body: 'Thanks for taking part in this demo - Nina'
+    };
+    client.sms.messages.create(twilioSms);
+
+    res.send(200);
+};
+app.get('/sms', smsHandler);
+app.post('/sms', smsHandler);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
